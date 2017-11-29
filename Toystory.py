26635c6a9425092b0a9bf2cab2d013680505c5f0
JasonBranch <jbranch2020@gmail.com>
2017-11-29 15:47:45 -0500

    Python game
    This is a game made in python based off of the non existent Toy Story 5. You can either be Buzz or Woody. Woody has to find Buzz if you choose Woody or if you choose Buzz you have to find Woody. You run into a series of characters along the way.

---
+++ Toystory.py
@@ -0,0 +1,282 @@
+global feeling
+global pop
+#all graphics
+ 
+def printgraphic(name):
+	if (name == "buzz"):
+
+
+		print'     __...-=--=-=--=---=---=-..__												'
+		print' .,--~                            ~~--.._										'
+		print'                .,;;!!!!!!!;;,,,.       `--.__ 								'
+		print'            ;;!!.,,,,.````````''';'											'
+		print'        ;;!!.,,,,.````````;;,     --------									'
+		print'       ;!!! d$$ccJ".d$$$$$hc ?$$$yerppppppp  .,,.""=c $       `-.				'
+		print'     .!!!! $$$$F <,,      h $$$$$F-?????$$c,dd           `.					'
+		print'    ,c.!!!!,$$$$ ?$$        $ 3$$$F       34 sdd"              `				'
+		print'  .c$$$ :! <$$$$L "$L     ,c$ d$$$Fadd    dddJ$$             					'
+		print'  $$$$$ !!!!$$$$$$c,`"????"",cP?$$L  ==ccc$$Pp                 `.				'
+		print'  $$$$$c !$$$$$$$$$hccccd$$$L;$$$h  c,,.,cc                    `.				'
+		print'  ?$$$$,! J$$$$$$$$$$$$$$$$$$$$P"",c,`$$$$$$h                     :			'
+		print'   3$$4$$$$$$$$$$$$$$$$$$$$$F,$$$$,$$$$$$$L.                    :				'
+		print'     ;;!.$$$$$$$$$$$$$$$$$$$$$$hcc:hh$$$$$$$$$$                     			'
+		print'    !!! $$$$$$$$$$$$$$Ff???????????$$$$$$$$$$$$$                    			'
+		print'     !! f$$$$$$$$$$$$L,c  .,,,,,,,,,,. ?$$$$$$$$$                      `.		'
+		print'     !>.$$$$$$$$$$$$$$$$L   `````````  J$$$$$$$$$.                       		'
+		print'     !$$$$$$$$$$$$$$$$$$ `  yerpppppp  J$$$$$$$$$                       :		'
+		print'    !! $$$$$$$$$$$$$$$$$$$$$$$$$yerppp??$$$$$$$$$                     . z$	'
+		print'    <!! $$$$$$$$$$$$$$$$$$$$$$$yerpppp$$$$$$$$$$                   d$F.$$		'
+		print'    !!! 3$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$       ;;;;!!!!!! ,$P"4$$	'
+		print'    !!! `$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ ,;!!!!!!!!!!! ,xnnnn 		'
+		print'    !!!! $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$P"?$$$$$$$ !!!!!!!,xdMMMMMMP ;!!!		'
+		print'   ;!!!!!!$$$$$$$$$$$$$$$$$$$$$$$$$$$$ yerp`3$$$$F,!!!,nnMMMMMMMP ;!!!!!		'
+		print'   !!!!!! ?$$$$$$$$$$$$$$$$$$$$$$$$$$$hcccJ$$$PP ,nMMMMMMMMMMMMP ;!!!!!!!!	'
+		print'   !!!!!!> "$$$$$$$$$$$$$$$$$$$$$$$$$$$$P""",xnMMMMMMMPPP""".  <!!!!!!!!!		'
+		print'  ;!!!!!!!!; "?$$$$$$$$$$$$$$$$$P"".,nmdMMM??..;;;;<!!!!! <!!!!!!!!!			'
+		print'  !!!!!!!!!!!;,.`""???$$$??""???????????????!!!!!!!!!!!!` <!!!!!!!!! ,		'
+		print'  !!!!!!!!!!!!!!!!''...-="...;;>-.    ;;  !!!!!!!!!! .<!!!!!!!!!! !!!!!		'
+		print' !!!!!!!!!!!``  ,-="..;;;;;..;;.    -'  '										' 
+		print'				BUZZ LIGHTYEAR HEAR TO SAVE      								'
+
+	if (name == "potatohead"):
+		print'                .-" "-.              '
+		print'               |       |             '
+		print'             (|-._____.-|)           '
+		print'          ..  |-._____.-|  ..        '
+		print'        . , :./|.== ==.|\.: , .      '
+		print'       : (  :   ___ ___   :  ) ;     '
+		print'       .._.:    |0| |0|    :._..     '
+		print'          /     ' '_' '     \        '
+		print'        _.|       / \       |.       '
+		print'      ...-|      (   )      |-...    '
+		print'     //   |  .-".".- "."-.  |   //   '
+		print'    ||    |  `~":-...-:"~`  |    ||  '
+		print'    ||     \.    '   '    ./     ||  '
+		print'    ||        -._     _.-        ||  '
+		print'   Mr. Potatohead here to hel..hahaha'
+
+
+	if (name == "Martian"):
+		print'		       	     ()						'
+		print'                 __/\__					'      
+		print'        |\   .-"`      `"-.   /| 			'
+		print'        | \."( '') ( '') (. )./|			'
+		print'         \_                  _/			'
+		print'           \    . :::: .    /				'
+		print'    ,       \..__      __../    			'   
+		print' .---!\________( .""~~"" )________/!---.	'
+		print' >   )       / `""~~~~""` \       (   <	'
+		print' ----`--..__/        -(-)- \__..--`----	'
+		print'            |_____ __ _____|				'
+		print'            [_____[##]_____]				'
+		print'            |              |				'
+		print'            \      ||      /				'
+		print'             \     ||     /				'
+		print'          .- ~ --._||_.-- ~ -.			'			
+		print'         (_____,.--""--.,_____)			'
+		print'		  The Matian is here to help       	'
+
+	if (name == "Slinky"):
+		print'			       																'
+		print'		       .- 			.													'
+		print'		   _,.'	   	        '.,													'
+		print'		  / /  .___. .___.  \ \													'
+		print'		 / /   ( o ) ( o )   \ \                                            ____'
+		print'		/ /|    ' '___' '    |\ \                                          (_)	'
+		print'		| |`\_,.-''   ''-.,_/ | |                                          /|	'
+		print'		| |  \             /  | |                                         /\;	'
+		print'		| |   \           /   | | _                              ___     /\/	'	
+		print'		| |    \   __    /\   | |'' `\-.-.-.-.-.-.-.-.-.-.-.-.-./''   -,/\/		'
+		print'		| |     \ (__)  /\ `- | |    `\ \ \ \ \ \ \ \ \ \ \ \ \`\       \/		'	
+		print'		| |      \-...-/  `-,_| |      \`\ \ \ \ \ \ \ \ \ \ \ \ \       \		'
+		print'		| |       '   '    /  | |       | | | | | | | | | | | | | |       |		'	
+		print'		| |               |   | |       | | | | | | | | | | | | | |       |		'
+		print'		\_/               |   \_/ .--.  | | | | | | | | | | | | | | .--.  |		'
+		print'		                  |       |  |  | | | | | | | | | | | | | | |  | /		'
+		print'		                   \      |  | / / / / / / / / / / / / / /  |  |/		'
+		print'		          																'
+
+
+
+
+ 		
+
+	if (name == "title"):
+	    print'   ______  _____    __    __      ____    ______  _____   ____    __    __      ____    ______  __  __  ____        '    
+	    print'  /\__  _\/\  __`\ /\ \  /\ \    /\  _`\ /\__  _\/\  __`\/\  _`\ /\ \  /\ \    /\  _`\ /\__  _\/\ \/\ \/\  _`\      '
+	    print'  \/_/\ \/\ \ \/\ \\ `\`\\///    \ \,\L\_\/_/\ \/\ \ \/\ \ \ \L\ \ `\`\\//     \ \ \L\_\/_/\ \/\ \ \ \ \ \ \L\_\    '
+	    print'     \ \ \ \ \ \ \ \`\ `\ /''     \/_\__ \  \ \ \ \ \ \ \ \ \ ,  /`\ `\ /       \ \  _\/  \ \ \ \ \ \ \ \ \  _\L    '
+	    print'      \ \ \ \ \ \ \_\ \ `\ \        /\ \L\ \ \ \ \ \ \ \_\ \ \ \\ \ `\ \ \       \ \ \/    \_\ \_\ \ \_/ \ \ \L\ \  '
+	    print'       \ \_\ \ \_____\  \ \_\       \ `\____\ \ \_\ \ \_____\ \_\ \_\ \ \_\       \ \_\    /\_____\ `\___/\ \____/  '
+	    print'        \/_/  \/_____/   \/_/        \/_____/  \/_/  \/_____/\/_/\/ /  \/_/        \/_/    \/_____/`\/__/  \/___/   '
+
+
+
+	if (name == "theend"):
+		print'		 gggggg   aa aa mm mm mmmm    eee     oooo  vv   vv   eee  rr rr  	'
+		print'		gg   gg  aa aaa mmm  mm  mm ee   e   oo  oo  vv vv  ee   e rrr  r 	'
+		print'		ggggggg aa  aaa mmm  mm  mm eeeee    oo  oo   vvv   eeeee  rr     	'
+		print'		     gg  aaa aa mmm  mm  mm  eeeee    oooo     v     eeeee rr  		'   
+		print'		 ggggg 																'
+	
+def escape():
+	print 'Buzz: Woo I thought I would never find you'
+	print 'Woody: I am so happy to see you Buzz'
+	print 'Lets get out of here'
+
+
+def Slinky():
+	print'Slink is here to save the day'
+	print'He lifts you to the couch and onto the window'
+	escape()
+
+
+
+def Martian():
+	print'You cant go to the window'
+	print'Sid is at the door hell see you'
+	print'HIDE'
+	raw_input("press enter >")
+	print'The Martian takes you to the couch but you cant get up.'
+	Slink()
+
+def gameover():
+
+	global pop
+
+	print 'You woke up Buster the dog'
+	print 'Buster *growls loudly'
+	print 'Buster the dog is tearing you apart'
+	print 'Your leg is gone'
+	print 'Your arm is gone'
+	print 'RIP "pop: "'
+	printgraphic ("theend")
+
+	print "pop: " + "This isnt over"
+	print " Your quest will continue in..."
+	print " Toy story 6"
+
+def buzzstory():
+	raw_input("press enter >")
+    print "There is a noise down the hall but you go forward"
+    print "You stop when you see a shadown in the distance"
+    printgraphic ("potatohead")
+    raw_input("press enter >")
+
+    print "You consider your options."
+    print "options: [ ask for help , turn around , go elsewhere]"
+
+    route = raw_input(">")
+
+    if (route == "ask for help"): 
+        print "You ask for help..."
+        print "Mr.Potatohead seems craft"
+        print "But he agrees to help"
+
+    if (route == "turn around"): 
+    	gameover()
+
+    if (route == "go elsewhere"): 
+	    print "You ask for help..."
+	    print "Mr.Potatohead seems craft"
+	    print "But he agrees to help"
+
+
+
+
+
+
+def woodystory():
+	#firstsequence
+
+	printgraphic("woodypicture")
+	print'You need to make your way out of Sids room, downstairs and then to the window'
+	print'Sids brother is going to be home in 15 minutes'
+	print'Its 4:30 now'
+	print'Press enter to leave the room'
+	print'The Martian appears'
+	print'He is here to help'
+	print'You still need to get downstairs'
+	print'He can teleport you'
+	print'Sneak past the dog at the bottom of the steps'
+	print'You can Walk down the the stairs, slide down the handrail, Martian'
+	print'walk, slide, teleport'
+	pathway = raw_input(">")
+	#secondsequence
+	#firstchoice
+
+
+	if (pathway == "walk"):
+		gameover()
+	
+
+	#secondchoice #almsotescpae
+
+	elif (pathway == "slide"):
+		print 'Woo'
+		print 'That was close Woody'
+		print 'You made it down the steps'
+		print 'Nows your chance to escape but you forgot something'
+		print 'You left your hat upstairs'
+		print 'Buzz is expecting to see you at the porch window'
+		print 'Options: go to the window or back upstairs'
+		secondchoice = raw_input(">") #user input
+
+	elif(pathway == "Martian"):
+		Martian()
+
+	#option 2
+	#elif (secondchoice == "back upstairs")
+
+
+
+def woodyescape():
+ 
+	print "name: " + "This isnt over"
+	print "You must still save" + "opposite: "
+	print " Your quest will continue in"
+	print " Toy story 6"
+	return
+
+def introstory():
+
+	#beginning of story
+	print "Toy Story 6.....loading"
+	print "Which charcter are you? Buzz or Woody"
+
+	pop = raw_input("Buzz or Woody >")
+	print "Do you feel brave or scared?"
+
+	feeling = raw_input(">")
+	raw_input("press enter")
+
+	
+	
+	#player can choose buzz or woody
+
+	if 	(pop == "Buzz"):
+		printgraphic ("buzz")
+		print "pop: " ".Woody is lost in Sids house"
+		print "You need to rescue him before Sid comes home and tears him apart"
+		print "The store closes in 15 minutes. Save Woody and save the day."
+		print "You may have some help along the way along with some distractions"
+		print "Choose wisely Buzz"
+		print'Enter Sids house'
+
+		buzzstory() #buzz version of the story
+
+	if (pop == "Woody"):
+		print "Woody, Wake up."
+		print "You're trapped in Sids house. You need to escape before he returns"
+		print "Make your way to the front porch so Buzz can find you."
+		print "If you see Mr. Potato beware"
+		woodystory() #woodys version of the story
+
+
+
+# main! most programs start with this.
+def main():
+    printgraphic("title") # call the function to print title image
+    introstory() # start the intro
+
+main() # this is the first thing that happens
